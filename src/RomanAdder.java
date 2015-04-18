291550e196bc525215393fccbaaaab5054000109
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 18:17:02 +0200

    Passed TC1: add returns "II"

--- src/RomanAdder.java
+++ src/RomanAdder.java
@@ -1,8 +1,5 @@
-/**
- * Created by khk on 18-Apr-15.
- */
 public class RomanAdder {
    public String add(String a, String b) {
-      return null;
+      return "II";
    }
 }
--- test/RomanAdderTest.java
+++ test/RomanAdderTest.java
@@ -3,9 +3,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by khk on 18-Apr-15.
- */
 public class RomanAdderTest {
    RomanAdder adder;
 
