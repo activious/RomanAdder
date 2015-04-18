42145dbf2a53d69dd928460f6e0e214ec3834ac2
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 19:16:14 +0200

    Passed TC4; I and V numerals are output in correct order

--- src/RomanAdder.java
+++ src/RomanAdder.java
@@ -1,10 +1,15 @@
-public class RomanAdder {
-   private enum Numeral {
-      M, D, C, L, X, V, I
-   }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class RomanAdder {
    public String add(String a, String b) {
-      String res = a + b;
+      Pattern p = Pattern.compile("(V*)(I*)");
+      Matcher mA = p.matcher(a), mB = p.matcher(b);
+      String res = "";
+      if (!mA.matches() || !mB.matches())
+         return null;
+      res += mA.group(1) + mB.group(1);
+      res += mA.group(2) + mB.group(2);
       res = res.replaceFirst("I{5}", "V");
       return res;
    }
