3b1d383031b46ea2c768a76d71c40a61acda3a4d
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 19:23:31 +0200

    Passed TC6; Now handles I, V, and X correctly

--- src/RomanAdder.java
+++ src/RomanAdder.java
@@ -3,13 +3,14 @@ import java.util.regex.Pattern;
 
 public class RomanAdder {
    public String add(String a, String b) {
-      Pattern p = Pattern.compile("(V*)(I*)");
+      Pattern p = Pattern.compile("(X*)(V*)(I*)");
       Matcher mA = p.matcher(a), mB = p.matcher(b);
       String res = "";
       if (!mA.matches() || !mB.matches())
          return null;
       res += mA.group(1) + mB.group(1);
       res += mA.group(2) + mB.group(2);
+      res += mA.group(3) + mB.group(3);
       res = res.replaceFirst("I{5}", "V");
       res = res.replaceFirst("V{2}", "X");
       return res;
