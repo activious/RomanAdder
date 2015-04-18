f28b80b59b220d6558b9b84738028484c84a58a2
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 19:59:23 +0200

    Refactored pattern building, now dynamic

--- src/RomanAdder.java
+++ src/RomanAdder.java
@@ -2,8 +2,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RomanAdder {
+   private static final String[] NUMERALS = { "M", "D", "C", "L", "X", "V", "I" };
+   private static Pattern p;
+
+   static {
+      p = Pattern.compile("(" + String.join("*)(", NUMERALS) + "*)");
+   }
+
    public String add(String a, String b) {
-      Pattern p = Pattern.compile("(M*)(D*)(C*)(L*)(X*)(V*)(I*)");
       Matcher mA = p.matcher(a), mB = p.matcher(b);
       if (!mA.matches() || !mB.matches())
          return null;
