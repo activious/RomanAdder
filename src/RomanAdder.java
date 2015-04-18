cc4a00019bf42d62984fd242089c0836e78dbabf
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 18:13:53 +0200

    Implemented TC1: I + I = II

---
+++ src/RomanAdder.java
@@ -0,0 +1,8 @@
+/**
+ * Created by khk on 18-Apr-15.
+ */
+public class RomanAdder {
+   public String add(String a, String b) {
+      return null;
+   }
+}
--- test/RomanAdderTest.java
+++ test/RomanAdderTest.java
@@ -1,7 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by khk on 18-Apr-15.
  */
 public class RomanAdderTest {
+   RomanAdder adder;
+
+   @Before
+   public void setup() {
+      adder = new RomanAdder();
+   }
+
+   @Test
+   public void testIplusIisII() {
+      String res = adder.add("I", "I");
+      assertEquals("II", res);
+   }
 }
