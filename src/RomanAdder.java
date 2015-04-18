94cc30fbe448062348b44f94dbbdff25930eeef8
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 19:29:04 +0200

    Passed TC7; XXXXX is reduced to L

--- src/RomanAdder.java
+++ src/RomanAdder.java
@@ -13,6 +13,7 @@ public class RomanAdder {
       res += mA.group(3) + mB.group(3);
       res = res.replaceFirst("I{5}", "V");
       res = res.replaceFirst("V{2}", "X");
+      res = res.replaceFirst("X{5}", "L");
       return res;
    }
 }
