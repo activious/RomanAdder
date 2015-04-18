c41f9dd7f7b33abd2e4a716e73728fff8429c9d8
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-04-18 20:01:03 +0200

    Refactored construction of result string, now dynamic

--- src/RomanAdder.java
+++ src/RomanAdder.java
@@ -13,14 +13,11 @@ public class RomanAdder {
       Matcher mA = p.matcher(a), mB = p.matcher(b);
       if (!mA.matches() || !mB.matches())
          return null;
+
       String res = "";
-      res += mA.group(1) + mB.group(1);
-      res += mA.group(2) + mB.group(2);
-      res += mA.group(3) + mB.group(3);
-      res += mA.group(4) + mB.group(4);
-      res += mA.group(5) + mB.group(5);
-      res += mA.group(6) + mB.group(6);
-      res += mA.group(7) + mB.group(7);
+      for (int i = 1; i <= NUMERALS.length; i++)
+         res += mA.group(i) + mB.group(i);
+
       res = res.replaceFirst("I{5}", "V");
       res = res.replaceFirst("V{2}", "X");
       res = res.replaceFirst("X{5}", "L");
